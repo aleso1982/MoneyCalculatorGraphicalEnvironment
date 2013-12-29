8c6eaffbe1014551b0cd2ec97276353c19a02844
alejandrosuarezocampo <alejandrosuarezocampo@192.168.1.135>
2013-12-29 20:56:18 +0000

    MoneyCalculator con entorno gráfico

---
+++ src/moneycalculator/MoneyPanel.java
@@ -0,0 +1,18 @@
+package moneycalculator;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+class MoneyPanel extends JPanel {
+
+    public MoneyPanel() {
+        this.add(createTextField());
+        this.add(new CurrencyPanel());
+    }
+
+    private JTextField createTextField() {
+        JTextField textField = new JTextField(10);
+        return textField;
+    }
+
+}
